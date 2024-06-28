c2cb7808be772763a1064515a1ea7ccaad0c7bb1
igor <igorpanchenko94@yandex.ru>
2024-06-28 23:21:22 +0300

    добавлена пагинация, возможность делиться постами, комментировать

---
+++ blog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from blog.models import Comment
+
+
+class EmailFormPost(forms.Form):
+    name = forms.CharField(label='Ваше имя', max_length=25)
+    email = forms.EmailField(label='Ваша почта')
+    to = forms.EmailField(label='Адрес получателя')
+    comment = forms.CharField(required=False, widget=forms.Textarea, label='Комментарий', max_length=100)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = 'Имя'
+        self.fields['email'].label = 'Почта'
+        self.fields['body'].label = 'Коментарий'
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,89 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.views.generic import ListView
+from django.views.decorators.http import require_POST
+from django.core.mail import send_mail
+from blog.forms import EmailFormPost, CommentForm
 from blog.models import Post
 
 
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
+
 def post_list(request):
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {
-        'posts': Post.published.all()
+        'posts': posts
     })
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request, 'blog/post/detail.html', {
-        'post': post
+        'post': post,
+        'form': form,
+        'comments': comments,
     })
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailFormPost(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"рекомендую прочитать {post.title}"
+            )
+            message = (
+                f"Прочните {post.title} на {post_url}\n\n"
+                f"{cd['name']}\' считает что: {cd['comment']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
+    else:
+        form = EmailFormPost()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)  # определить, но не сохранять, чтобы модифицировать к финальному сохранению
+        comment.post = post
+        comment.save()
+        return render(request, 'blog/comment.html', {'post': post, 'form': form, 'comment': comment})
