b3fd67abd9faee43476902d73ca8b1aec4be92ad
igor <igorpanchenko94@yandex.ru>
2024-07-02 19:40:21 +0300

    Добавлен RSS

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostsFeed
 
 from . import views
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
